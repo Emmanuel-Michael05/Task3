bf8a597defd09c7207d4c968ed5e0d34b3f06fe5
Emmanuel Michael Ikechukwu <162467554+Emmanuel-Michael05@users.noreply.github.com>
2024-11-08 14:24:38 +0100

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loop from 1 to 10
+            for (int i = 1; i <= 10; i++)
+            {
+                // Print each number
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
